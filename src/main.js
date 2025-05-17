d636a4e2b1a64d310844a8b0b54ea5b9fd28acec
ThamasMc <thamas_mc@yahoo.com>
2025-05-16 20:01:52 -0400

    Have some fun adding basic entity array

--- src/main.js
+++ src/main.js
@@ -15,8 +15,76 @@ let square = {
     x: 50,
     y: 50
   },
+  velocity: {
+    x: 300,
+    y: 500
+  },
   color: "rgb(255 0 0)"
 };
+
+let square1 = {
+  position: {
+    x: 0,
+    y: 0,
+  },
+  scale: {
+    x: 25,
+    y: 25
+  },
+  velocity: {
+    x: 150,
+    y: 250
+  },
+  color: "rgb(255 255 0)"
+};
+
+const getRandomInt = (max) => Math.floor(Math.random() * max);
+const getRandomColor = () => `rgb(${getRandomInt(256)} ${getRandomInt(256)} ${getRandomInt(256)})`;
+
+let randRect = {
+  position: {
+    x: getRandomInt(500),
+    y: getRandomInt(500),
+  },
+  scale: {
+    x: getRandomInt(200),
+    y: getRandomInt(200)
+  },
+  velocity: {
+    x: getRandomInt(600),
+    y: getRandomInt(600)
+  },
+  color: "rgb(0 0 255)"
+};
+
+let entities = [square, square1, randRect];
+
+const generateRects = (num) => {
+  for(let i = 0; i < num; i++) {
+    let rand = {
+      position: {
+        x: getRandomInt(500),
+        y: getRandomInt(500),
+      },
+      scale: {
+        x: getRandomInt(200),
+        y: getRandomInt(200)
+      },
+      velocity: {
+        x: getRandomInt(600),
+        y: getRandomInt(600)
+      },
+      color: getRandomColor()
+    };
+
+    // console.log(rand.position, rand.scale, rand.velocity, rand.color);
+    entities.push(rand)
+
+  }
+};
+
+generateRects(5000);
+// console.log(randRect.position, randRect.scale, randRect.velocity, randRect.color);
 
 const draw = () => {
   width = (canvas.width = window.innerWidth);
@@ -25,45 +93,53 @@ const draw = () => {
   ctx.fillStyle = "rgb(0 0 0)";
   ctx.fillRect(0, 0, width, height);
 
-  ctx.fillStyle = square.color;
-  ctx.fillRect(square.position.x, square.position.y, square.scale.x, square.scale.y);
+  for (const entity of entities) {
+    // TODO: Add component check
+
+    ctx.fillStyle = entity.color;
+    ctx.fillRect(entity.position.x, entity.position.y, entity.scale.x, entity.scale.y);
+  }
+
 }
 
-let velocity = {
-  x: 100,
-  y: 0
-};
-
 const move = (deltaTime) => {
-  const deltaMod = deltaTime / 1000;
-  /*
-   * 1000px moving 10px p second, should take 100 seconds
-   */
+  for (const entity of entities) {
+    // TODO: Update to transform component
+    if (entity.velocity === undefined) {
+      continue;
+    }
 
-  // Handle bounces
-  if((velocity.x + square.position.x + square.scale.x) > canvas.width) {
-    velocity.x = velocity.x * -1;
+    let velocity = entity.velocity
+    const deltaMod = deltaTime / 1000;
+    /*
+     * 1000px moving 10px p second, should take 100 seconds
+     */
+
+    // Handle bounces
+    if(((velocity.x * deltaMod) + entity.position.x + entity.scale.x) > canvas.width) {
+      velocity.x = velocity.x * -1;
+    }
+
+    if(((velocity.x * deltaMod) + entity.position.x) < 0) {
+      velocity.x = velocity.x * -1;
+    }
+
+    if(((velocity.y * deltaMod) + entity.position.y + entity.scale.y) > canvas.height) {
+      velocity.y = velocity.y * -1;
+    }
+
+    if(((velocity.y * deltaMod) + entity.position.y) < 0) {
+      velocity.y = velocity.y * -1;
+    }
+
+    entity.position.x += (velocity.x * deltaMod);
+    entity.position.y += (velocity.y * deltaMod);
   }
-
-  if((velocity.x + square.position.x) < 0) {
-    velocity.x = velocity.x * -1;
-  }
-
-  if((velocity.y + square.position.y + square.scale.y) > canvas.height) {
-    velocity.y = velocity.y * -1;
-  }
-
-  if((velocity.y + square.position.y) < 0) {
-    velocity.y = velocity.y * -1;
-  }
-
-  square.position.x += (velocity.x * deltaMod);
-  square.position.y += (velocity.y * deltaMod);
 }
 
 const gameLoop = (time) => {
   delta = time - current;
-  console.log(time, current, delta);
+  // console.log(time, current, delta);
   current = time;
   move(delta);
   draw();
